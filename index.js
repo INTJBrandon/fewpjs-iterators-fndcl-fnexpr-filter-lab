7858701f76d94a255583c6e20b063ca57257b154
seireitou <ausis@mail.com>
2021-04-29 00:31:02 -0500

    commit tree f3a0229e43d9fa61e81b7752c545fac9673d193c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+    return arr.filter(d => d.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(d => d[0] === string[0])
+}
+
+function matchName(arr, string) {
+    return arr.filter(d => d.name === string)
+}
